c2a75c079bc5282a2a190b6a3ec17039efc3c677
DominicFrancois <domofrancois@gmail.com>
2017-03-20 17:25:38 +0000

    Print password to screen
    Prints password to text file and also to the screen.

--- vanessa-password.py
+++ vanessa-password.py
@@ -2,11 +2,11 @@ import string
 from random import sample, choice
 
 chars = string.ascii_letters + string.digits
-lenght = 20
+lenght = 10
 gen = ''.join(choice(chars) for _ in range(lenght))       
 plog = 'plog.txt'
 
 with open(plog, 'a') as out:
-    out.write(gen + '\n')
+   	out.write(gen + '\n')
 
-print("Password generated")
+print("Password generated: " + gen)
